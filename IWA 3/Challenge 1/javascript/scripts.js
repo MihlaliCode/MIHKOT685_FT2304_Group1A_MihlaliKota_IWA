9641e423e1be5ea939eee388b5f0f631fa6522b2
MihlaliCode <133135575+MihlaliCode@users.noreply.github.com>
2023-06-21 22:55:51 +0200

    Add files via upload

---
+++ IWA 3/Challenge 1/javascript/scripts.js
@@ -0,0 +1,10 @@
+// configuration.js
+const company = 'ACME Inc.';
+const year = 2022;
+
+// scripts.js
+import { company } from 'configuration';
+import { year } from 'configuration';
+
+const message = `© ${company} (${year})`;
+document.querySelector('footer').textContent = message;
