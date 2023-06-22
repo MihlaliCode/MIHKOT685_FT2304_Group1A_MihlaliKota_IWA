2ea6855a4e8bb57dc534777eb2d021944234bf90
MihlaliCode <hlalikota@gmail.com>
2023-06-22 03:21:22 +0200

    Second Update

--- IWA 3/Challenge 1/scripts.js
+++ IWA 3/Challenge 1/scripts.js
@@ -1,10 +1,8 @@
-// configuration.js
-const company = 'ACME Inc.';
-const year = 2022;
-
 // scripts.js
-import { company } from 'configuration';
-import { year } from 'configuration';
 
-const message = `© ${company} (${year})`;
-document.querySelector('footer').textContent = message;
+import { company, year } from './configuration.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const message = '© ' + company + ' (' + year + ')';
+  document.querySelector('footer').innerText = message;
+})
