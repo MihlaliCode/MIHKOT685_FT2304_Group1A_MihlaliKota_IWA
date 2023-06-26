18f5ee6f5efb4aaa9e9a38e6bf0a05f82250c450
MihlaliCode <hlalikota@gmail.com>
2023-06-26 23:50:43 +0200

    Code Update

--- IWA5/javascript/script.js
+++ IWA5/javascript/script.js
@@ -1,9 +1,10 @@
 const FREE_WARNING = "Free shipping only applies to single customer orders";
-const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
+const BANNED_WARNING =
+  "Unfortunately we do not ship to your country of residence";
 let NONE_SELECTED = 0;
 let shipping;
 let currency;
-let country = "RSA";
+let country = "NAM";
 let customers = 1;
 
 let shoes = 300 * 1;
@@ -11,6 +12,8 @@ let toys = 100 * 5;
 let shirts = 150 * NONE_SELECTED;
 let batteries = 35 * 2;
 let pens = 5 * NONE_SELECTED;
+
+let shippingItems = shoes + toys + shirts + batteries + pens;
 
 if (country === "RSA") {
   shipping = 400;
@@ -23,15 +26,22 @@ if (country === "RSA") {
   currency = "$";
 }
 
-
-if (shoes + batteries + pens + shirts > R1000 && $60 ) {
-	if (country = NAM && customers < 2) {
-			if (country = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+if (
+  (shippingItems >= 1000 && country === "RSA") ||
+  (shippingItems >= 60 && country === "NAM")
+) {
+  if (customers === 1) {
+    shipping = 0;
+  } else {
+    console.log(FREE_WARNING);
+  }
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) } */
 
-country === "NK" ? console.log(BANNED_WARNING) : console.log("price", currency, shoes + batteries + pens + shirts + shipping)
+country === "NK"
+  ? console.log(BANNED_WARNING)
+  : console.log(
+      "price",
+      currency,
+      shippingItems + shipping
+    );
