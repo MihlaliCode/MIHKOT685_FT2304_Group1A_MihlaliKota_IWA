89865aa16df38299aad26aaa4435655fa03ab213
MihlaliCode <hlalikota@gmail.com>
2023-07-04 03:16:36 +0200

    Update

--- IWA7/Challenge 3/javascript/script.js
+++ IWA7/Challenge 3/javascript/script.js
@@ -2,7 +2,7 @@ const leoName = "Leo";
 const leoSurname = "Musvaire     ";
 const leoBalance = "-9394";
 
-const sarahName = "Sarah";
+const sarahName = "Sarah    ";
 const sarahSurname = "Kleinhans";
 const sarahBalance = "-4582.21000111";
 
@@ -13,13 +13,17 @@ const divider = "----------------------------------";
 const owed =
   parseFloat(Math.abs(leoBalance).toFixed(2)) +
   parseFloat(Math.abs(sarahBalance).toFixed(2));
-const leo = `${leoName} ${leoSurname} (Owed: R ${Math.abs(leoBalance).toFixed(
-  2
-)})`;
-const sarah = `${sarahName} ${sarahSurname} (Owed: R ${Math.abs(
+const leo = `${leoName} ${leoSurname.trim()} (Owed: R ${Math.abs(
+  leoBalance
+).toFixed(2)})`;
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: R ${Math.abs(
   sarahBalance
 ).toFixed(2)})`;
 const total = "Total amount owed: R ";
-const result = `\n${leo}\n${sarah}\n${divider}\n${total}${owed}\n${divider}`;
+const result = `${leo}       
+${sarah}
+${divider}
+  ${total}${owed.toLocaleString("en-ZA")}
+${divider}`;
 
 console.log(result);
