4cafa613d14ae0f3b83a7939444cb22c866575d2
MihlaliCode <hlalikota@gmail.com>
2023-07-19 08:46:47 +0200

    Update

--- IWA_16/javascript/script.js
+++ IWA_16/javascript/script.js
@@ -1,19 +1,19 @@
 // scripts.js
 
 const MONTHS = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec',
-]
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
 const data = {
   response: {
@@ -28,11 +28,11 @@ const data = {
         id: "NM372",
         races: [
           {
-            date: '2022-11-18T20:00:00.000Z',
+            date: "2022-11-18T20:00:00.000Z",
             time: [9, 7, 8, 6],
           },
           {
-            date: '2022-12-02T20:00:00.000Z',
+            date: "2022-12-02T20:00:00.000Z",
             time: [6, 7, 8, 7],
           },
         ],
@@ -44,19 +44,19 @@ const data = {
         id: "SV782",
         races: [
           {
-            date: '2022-11-18T20:00:00.000Z',
+            date: "2022-11-18T20:00:00.000Z",
             time: [10, 8, 3, 12],
           },
           {
-            date: '2022-11-25T20:00:00.000Z',
+            date: "2022-11-25T20:00:00.000Z",
             time: [6, 8, 9, 11],
           },
           {
-            date: '2022-12-02T20:00:00.000Z',
+            date: "2022-12-02T20:00:00.000Z",
             time: [10, 11, 4, 8],
           },
           {
-            date: '2022-12-09T20:00:00.000Z',
+            date: "2022-12-09T20:00:00.000Z",
             time: [9, 8, 9, 11],
           },
         ],
@@ -68,44 +68,50 @@ const data = {
 // Only edit below this comment
 
 const createHtml = (athlete) => {
-  firstName, surname, id, races = athlete
-  [date], [time] = races.reverse()
+  const { firstName, surname, id, races } = athlete;
+  const [latestRace] = races.reverse();
+  const { date, time } = latestRace;
 
   const fragment = document.createDocumentFragment();
 
-  title = document.createElement(h2);
-  title= id;
+  const title = document.createElement("h2");
+  title.textContent = id;
   fragment.appendChild(title);
 
-  const list = document.createElement(dl);
+  const list = document.createElement("dl");
 
-  const day = date.getDate();
-  const month = MONTHS[date.month];
-  const year = date.year;
+  const raceDate = new Date(date);
+  const day = raceDate.getDate();
+  const month = MONTHS[raceDate.getMonth()];
+  const year = raceDate.getFullYear();
 
-  first, second, third, fourth = timeAsArray;
-  total = first + second + third + fourth;
-
-  const hours = total / 60;
-  const minutes = total / hours / 60;
+  const total = time.reduce((acc, curr) => acc + curr, 0);
+  const hours = Math.floor(total / 60);
+  const minutes = total % 60;
 
   list.innerHTML = /* html */ `
     <dt>Athlete</dt>
-    <dd>${firstName surname}</dd>
+    <dd>${firstName} ${surname}</dd>
 
     <dt>Total Races</dt>
-    <dd>${races}</dd>
+    <dd>${races.length}</dd>
 
     <dt>Event Date (Latest)</dt>
-    <dd>${day month year}</dd>
+    <dd>${day} ${month} ${year}</dd>
 
     <dt>Total Time (Latest)</dt>
-    <dd>${hours.padStart(2, 0) minutes}</dd>
+    <dd>${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}</dd>
   `;
 
   fragment.appendChild(list);
-}
 
-[NM372], [SV782] = data
-document.querySelector(NM372).appendChild(createHtml(NM372));
-document.querySelector(SV782).appendChild(createHtml(SV782));
+  return fragment;
+};
+
+const NM372 = data.response.data.NM372;
+const SV782 = data.response.data.SV782;
+
+document.querySelector('[data-athlete="NM372"]').appendChild(createHtml(NM372));
+document.querySelector('[data-athlete="SV782"]').appendChild(createHtml(SV782));
