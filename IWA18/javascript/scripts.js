0bdcc93e6f19e4265907ab383254234565753120
MihlaliCode <hlalikota@gmail.com>
2023-07-23 09:54:11 +0200

    Update

--- IWA18/javascript/scripts.js
+++ IWA18/javascript/scripts.js
@@ -1,3 +1,17 @@
+import {
+  TABLES,
+  COLUMNS,
+  state,
+  createOrderData,
+  updateDragging,
+} from "./data.js";
+import {
+  html,
+  createOrderHtml,
+  updateDraggingHtml,
+  moveToColumn,
+} from "./view.js";
+
 /**
  * A handler that fires when a user drags over any element inside a column. In
  * order to determine which column the user is dragging over the entire event
@@ -27,14 +41,130 @@ const handleDragOver = (event) => {
   updateDraggingHtml({ over: column });
 };
 
-const handleDragStart = (event) => {};
-const handleDragEnd = (event) => {};
-const handleHelpToggle = (event) => {};
-const handleAddToggle = (event) => {};
-const handleAddSubmit = (event) => {};
-const handleEditToggle = (event) => {};
-const handleEditSubmit = (event) => {};
-const handleDelete = (event) => {};
+const handleDragStart = (event) => {
+  const orderId = event.target.dataset.id;
+  updateDragging({ source: orderId });
+  event.dataTransfer.setData("text/plain", orderId);
+  event.target.classList.add("dragging");
+  event.dataTransfer.effectAllowed = "move";
+};
+
+const handleDragEnd = (event) => {
+  const orderId = event.target.dataset.id;
+  updateDragging({ source: null });
+  event.target.classList.remove("dragging");
+};
+
+const handleHelpToggle = (event) => {
+  const helpOverlay = html.help.overlay;
+
+  if (helpOverlay.open) {
+    helpOverlay.close();
+  } else {
+    helpOverlay.showModal();
+  }
+  html.other.add.focus();
+};
+
+const handleAddToggle = (event) => {
+  const addOverlay = html.add.overlay;
+  const addForm = html.add.form;
+
+  if (addOverlay.open) {
+    addOverlay.close();
+    addForm.reset();
+  } else {
+    addOverlay.showModal();
+  }
+  html.other.add.focus();
+};
+
+const handleAddSubmit = (event) => {
+  event.preventDefault();
+  const addForm = event.target;
+  const formData = new FormData(addForm);
+  const title = formData.get("title");
+  const table = formData.get("table");
+
+  if (!title || !table) {
+    return;
+  }
+  const newOrder = createOrderData({ title, table, column: "ordered" });
+  state.orders[newOrder.id] = newOrder;
+  const newOrderElement = createOrderHtml(newOrder);
+  html.columns.ordered.appendChild(newOrderElement);
+  html.add.overlay.close();
+  addForm.reset();
+};
+
+const handleEditToggle = (event) => {
+  const targetOrderElement = event.target.closest(".order");
+
+  if (!targetOrderElement) {
+    return;
+  }
+
+  const orderId = targetOrderElement.dataset.id;
+  const order = state.orders[orderId];
+
+  if (!order) {
+    return;
+  }
+  html.edit.title.value = order.title;
+  html.edit.table.value = order.table;
+  html.edit.id.value = orderId;
+  html.edit.column.value = order.column;
+  html.edit.overlay.showModal();
+};
+
+const handleEditSubmit = (event) => {
+  event.preventDefault();
+  const editForm = event.target;
+  const formData = new FormData(editForm);
+  const orderId = formData.get("id");
+  const order = state.orders[orderId];
+
+  if (!order) {
+    return;
+  }
+
+  const updatedTitle = formData.get("title");
+  const updatedTable = formData.get("table");
+  const updatedColumn = formData.get("column");
+
+  if (!updatedTitle || !updatedTable || !updatedColumn) {
+    return;
+  }
+
+  order.title = updatedTitle;
+  order.table = updatedTable;
+  order.column = updatedColumn;
+
+  const updatedOrderElement = createOrderHtml(order);
+
+  moveToColumn(orderId, updatedColumn);
+
+  html.edit.overlay.close();
+};
+
+const handleDelete = (event) => {
+  const editForm = event.target.closest("form");
+  const formData = new FormData(editForm);
+  const orderId = formData.get("id");
+  const order = state.orders[orderId];
+
+  if (!order) {
+    return;
+  }
+
+  delete state.orders[orderId];
+
+  const orderElement = document.querySelector(`[data-id="${orderId}"]`);
+  if (orderElement) {
+    orderElement.remove();
+  }
+  html.edit.overlay.close();
+};
 
 html.add.cancel.addEventListener("click", handleAddToggle);
 html.other.add.addEventListener("click", handleAddToggle);
